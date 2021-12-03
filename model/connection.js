81a8c7cc06227ed192879b24957eb7e5b74bdca9
Murilo Rainho <murilorainho01@gmail.com>
2021-12-03 08:18:33 -0300

    configuring .env

--- model/connection.js
+++ model/connection.js
@@ -1,10 +1,20 @@
 import { createPool } from 'mysql2/promise';
 
+import { config as dotenvConfig } from 'dotenv';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+dotenvConfig({
+  path: path.resolve(__dirname, './.env'),
+});
+
 const connection = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'murilo123',
-  database: 'dev_registration',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 export default connection;
